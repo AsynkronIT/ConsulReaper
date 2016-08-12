d663ff81e47931578622023e5de9092f1e3bd812
rogeralsing <rogeralsing@gmail.com>
2016-08-12 09:26:07 +0200

    working

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/consul/api"
+	"github.com/rogeralsing/goconsole"
 )
 
 func Reap(datacenter string) {
@@ -13,31 +14,38 @@ func Reap(datacenter string) {
 		log.Println("Cound not create Consul client")
 		return
 	}
+	for ; ; {
+		log.Println("Getting critical services")
+		criticalServices, _, err := client.Health().State("critical", &api.QueryOptions{
+			Datacenter:        datacenter,
+			AllowStale:        true,
+			RequireConsistent: false,
+			WaitTime:          5 * time.Second,
+		})
 
-	criticalServices, _, err := client.Health().State("critical", &api.QueryOptions{
-		Datacenter:        datacenter,
-		AllowStale:        true,
-		RequireConsistent: false,
-		WaitTime:          5 * time.Second,
-	})
-
-	if err != nil {
-		log.Println("Cound not get service health")
-		panic(err)
-		return
-	}
-	for _, critical := range criticalServices {
-		_, err := client.Catalog().Deregister(&api.CatalogDeregistration{
-			Datacenter: datacenter,
-			Node:       critical.Node,
-			ServiceID:  critical.ServiceID,
-		}, &api.WriteOptions{})
 		if err != nil {
-			log.Println("Cound not deregister " + critical.ServiceID)
+			log.Println("Cound not get service health")
+			return
 		}
+
+		log.Println("Got critical services")
+		for _, critical := range criticalServices {
+			log.Println("Deregistering " + critical.ServiceID)
+			_, err := client.Catalog().Deregister(&api.CatalogDeregistration{
+				Datacenter: datacenter,
+				Node:       critical.Node,
+				ServiceID:  critical.ServiceID,
+			}, &api.WriteOptions{})
+			if err != nil {
+				log.Println("Cound not deregister " + critical.ServiceID)
+			}
+		}
+
+		time.Sleep(10 * time.Second)
 	}
 }
 
 func main() {
-	Reap("")
+	go Reap("")
+	console.ReadLine()
 }
